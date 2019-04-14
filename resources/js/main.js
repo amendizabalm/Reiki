fc9bde41bee6e085cf1088fd2f7d4fbdc83593df
amendizabalm <44274378+amendizabalm@users.noreply.github.com>
2019-04-13 22:44:43 -0500

    responsive navbar finished

--- resources/js/main.js
+++ resources/js/main.js
@@ -17,3 +17,32 @@ $(document).ready(function(){
         });
     });
     //offset returns coordinates, top is the header
+    
+     //Use slick to set up carousel
+  var $galleryImg = $(".gallery img");
+  var $closeBtn = $(".btn");
+  var $carousel = $(".carousel");
+  var $overlay = $(".overlay");
+
+  $carousel.slick({
+    centerMode: true,
+    fade: true,
+    arrows: false,
+    dots: true,
+    draggable: false
+  });
+
+  $galleryImg.on("click", function(slick) {
+    var $imgId = $(this).attr("id");
+    // Kick off fade-in, specifically to set display: block
+    $overlay.fadeIn();
+    // Goto clicked image slide and ensure we trigger dimension calculation
+    $carousel.slick("slickGoTo", $imgId, true).slick("setPosition");
+  });
+
+  $closeBtn.on("click", function(event) {
+    event.preventDefault();
+    $overlay.fadeOut();
+  });
+});
+
